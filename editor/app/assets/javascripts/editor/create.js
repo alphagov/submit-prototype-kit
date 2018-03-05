69057e5d56649eaee74f29c14d967681020209e6
Tom Byers <tom.byers@digital.cabinet-office.gov.uk>
2018-03-05 16:35:56 +0000

    Make creation page work for all page types

--- editor/app/assets/javascripts/editor/create.js
+++ editor/app/assets/javascripts/editor/create.js
@@ -1,7 +1,56 @@
 (function (window, document) {
   var heading = document.getElementById('heading');
   var pageName = document.getElementById('page');
+  var fields = document.getElementById('specific-fields');
+  var pageSelector = document.getElementById('page-selector');
 
+  var updateFields = function () {
+    var pageType = document.getElementById('pagetype').value;
+    var fieldProps = {
+      'heading': {
+        'label': 'Page heading',
+        'template': 'govukInput'
+      },
+      'name': {
+        'label': 'Page name (used in URLs)',
+        'template': 'govukInput'
+      },
+      'guidance': {
+        'label': 'Page guidance',
+        'template': 'govukTextarea'
+      },
+      'detail': {
+        'label': 'Page detail',
+        'template': 'govukInput'
+      }
+    };
+    var pageFields = {
+      'start-page': ['heading','name', 'guidance'],
+      'question': ['heading', 'name'],
+      'check-your-answers': ['heading', 'name'],
+      'application-complete': ['heading','name', 'guidance', 'detail'],
+      'bounce': ['heading', 'name', 'guidance']
+    };
+    var fieldsForPage = pageFields[pageType];
+
+    fields.innerHTML = '';
+
+    fieldsForPage.forEach(field => {
+      var template = document.Editor.templates[fieldProps[field].template];
+      var data = {
+        'params': {
+          'name': field,
+          'id': field,
+          'label': { 'text': fieldProps[field].label }
+        }
+      };
+      var result = nunjucks.renderString(template, data);
+
+      fields.innerHTML += result;
+    });
+  };
+
+  // Make page name placeholder from heading field
   if (heading && pageName) {
     heading.addEventListener('keyup', function (evt) {
       if ((heading.value !== '') && (pageName.value === '')) {
@@ -9,4 +58,11 @@
       }
     }, false);
   }
+
+  pageSelector.querySelectorAll('button')[0].addEventListener('click', function (evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+
+    updateFields();
+  }, false);
 })(window, document);
